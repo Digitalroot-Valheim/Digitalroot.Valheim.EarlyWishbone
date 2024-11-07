c460e8839e99a0ca6adc1496aeddeb190e59d4e9
Digitalroot <digitalroot@gmail.com>
2024-11-07 04:17:19 -0800

    Updated for automated workflow.

--- src/Digitalroot.Valheim.EarlyWishbone/MetaData.cs
+++ src/Digitalroot.Valheim.EarlyWishbone/MetaData.cs
@@ -5,7 +5,7 @@ namespace Digitalroot.Valheim.EarlyWishbone
   [SuppressMessage("ReSharper", "MemberCanBePrivate.Global")]
   public partial class Main
   {
-    public const string Version = "1.0.0";
+    public const string Version = "0.0.1";
     public const string Name = "Early Wishbone";
     public const string Guid = "digitalroot.mods.earlywishbone";
     public const string Namespace = "Digitalroot.Valheim." + nameof(EarlyWishbone);
